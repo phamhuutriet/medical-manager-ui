436bb5052218c80ddb3ab51e546b5766d50b8a7d
Little Panda <triet.ph1999@gmail.com>
2024-01-24 00:45:57 +0700

    refactor: extract inline css into css file

--- src/mainPage/header/Header.tsx
+++ src/mainPage/header/Header.tsx
@@ -2,38 +2,13 @@ import React from "react";
 import { Avatar, Grid, IconButton } from "@mui/material";
 import NotificationIcon from "../../img/notification.svg";
 import DropDownIcon from "../../img/dropdown.svg";
+import "./index.css";
 
 export const Header = () => {
   return (
-    <Grid
-      item
-      xs={12}
-      sx={{
-        height: "7.8125vh",
-        paddingTop: "1px",
-        paddingBottom: "1px",
-        borderBottom: "1px solid #E5E7EB",
-      }}
-    >
-      <div
-        style={{
-          width: "95.83%",
-          marginLeft: "auto",
-          marginRight: "auto",
-          background: "white",
-          marginTop: "1.171875vh",
-          marginBottom: "1.171875vh",
-          height: "5.46875vh",
-        }}
-      >
-        <div
-          style={{
-            marginLeft: "80%",
-            display: "flex",
-            alignItems: "center",
-            height: "100%",
-          }}
-        >
+    <Grid item xs={12} className="header-grid-item">
+      <div className="header-container">
+        <div className="header-inner">
           <Grid container>
             <NotificationIconButton />
             <Grid item xs={1.5}></Grid>
@@ -48,23 +23,11 @@ export const Header = () => {
 
 const UserMenu = () => {
   return (
-    <Grid
-      item
-      xs={8.58}
-      sx={{
-        // background: "yellow",
-        justifyContent: "flex-start",
-        alignItems: "center",
-        gap: "6.55%",
-        display: "flex",
-      }}
-    >
+    <Grid item xs={8.58} className="user-menu">
       <IconButton sx={{ p: 0, marginLeft: "4.37%" }}>
         <Avatar />
       </IconButton>
-      <div style={{ fontSize: "1.5625vh", fontFamily: "Be Vietnam Pro" }}>
-        Triet Pham
-      </div>
+      <div className="text">Triet Pham</div>
       <img className="img" alt="dropdown-icon" src={DropDownIcon} />
     </Grid>
   );
