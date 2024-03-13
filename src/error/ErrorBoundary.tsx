143f16b9a25e336a74c903fd3c3ac2f44c8ed8a4
phamhuutriet <triet.ph1999@gmail.com>
2024-03-13 16:44:59 -0600

    feat: add ErrorModal

--- src/error/ErrorBoundary.tsx
+++ src/error/ErrorBoundary.tsx
@@ -1,29 +1,42 @@
-import React from "react";
+import React, { PropsWithChildren } from "react";
+import { ErrorModal } from "./ErrorModal";
 
 interface ErrorBoundaryState {
   hasError: boolean;
+  error: Error | undefined;
 }
 
 export class ErrorBoundary extends React.Component<
-  React.PropsWithChildren,
+  PropsWithChildren,
   ErrorBoundaryState
 > {
-  constructor(props: React.PropsWithChildren) {
+  constructor(props: PropsWithChildren) {
     super(props);
-    this.state = { hasError: false };
+    this.state = { hasError: false, error: undefined };
+    this.handleClose = this.handleClose.bind(this);
   }
 
-  // if an error happended, this will catch and set state to true
   static getDerivedStateFromError(error: Error) {
-    return { hasError: true };
+    return { hasError: true, error };
+  }
+
+  handleClose() {
+    this.setState({ hasError: false });
   }
 
   render() {
-    if (this.state.hasError) {
-      // TODO: implement an error component
-      return <>Has Error</>;
-    }
-
-    return this.props.children;
+    // Render the children inside a fragment and the modal
+    return (
+      <>
+        {this.props.children}
+        {this.state.hasError && (
+          <ErrorModal
+            open
+            handleClose={this.handleClose}
+            errorText={this.state.error ? this.state.error.message : "Error"}
+          />
+        )}
+      </>
+    );
   }
 }
---
+++ src/error/ErrorModal.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import { Box, Modal } from "@mui/material";
+import { ExclamationMark } from "../img/svg/ExclamationMark";
+import "./index.css";
+
+const style = {
+  position: "absolute" as "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  width: 400,
+  bgcolor: "background.paper",
+  border: "2px solid #000",
+  boxShadow: 24,
+  p: 4,
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "space-between",
+  alignItems: "center",
+};
+
+export const ErrorModal = ({
+  open,
+  handleClose,
+  errorText,
+}: {
+  open: boolean;
+  handleClose: any;
+  errorText: string;
+}) => {
+  return (
+    <Modal className="error-modal" open={open} onClose={handleClose}>
+      <Box sx={style}>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            gap: "12px",
+            fontFamily: "Be Vietnam Pro",
+          }}
+        >
+          <ExclamationMark />
+          <div style={{ fontSize: "20px", fontWeight: "600" }}>OOps !!</div>
+          <div
+            style={{ fontSize: "14px", fontWeight: "400", color: "#8C949D" }}
+          >
+            {errorText}
+          </div>
+        </div>
+      </Box>
+    </Modal>
+  );
+};
