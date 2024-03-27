94e56c7f64cc16a5f787c04b36767b9cf569e5c4
phamhuutriet <triet.ph1999@gmail.com>
2024-03-26 18:57:52 -0600

    refactor: refactor sex drop down

--- src/mainPage/doctor/doctor-detail/SexDropDown.tsx
+++ src/mainPage/doctor/doctor-detail/SexDropDown.tsx
@@ -1,55 +1,9 @@
 import * as React from "react";
-import { styled } from "@mui/material/styles";
-import Menu, { MenuProps } from "@mui/material/Menu";
-import MenuItem from "@mui/material/MenuItem";
-import { IconButton } from "@mui/material";
 import { ArrowDownIcon } from "../../../img/svg/ArrowDownIcon";
-
-const StyledMenu = styled((props: MenuProps) => (
-  <Menu
-    elevation={0}
-    anchorOrigin={{
-      vertical: "bottom",
-      horizontal: "right",
-    }}
-    transformOrigin={{
-      vertical: "top",
-      horizontal: "right",
-    }}
-    {...props}
-  />
-))(({ theme }) => ({
-  "& .MuiPaper-root": {
-    borderRadius: 12,
-    marginTop: theme.spacing(1),
-    width: "327px", // TODO: find a way to parameterize this
-    boxShadow:
-      "rgb(255, 255, 255) 0px 0px 0px 0px, rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px",
-    "& .MuiMenu-list": {
-      padding: "4px 0",
-    },
-    "& .MuiMenuItem-root": {
-      "&": {
-        fontSize: 16,
-        height: "56px",
-        color: "#8C949D",
-        gap: "12px",
-        fontFamily: "Be Vietnam Pro",
-        padding: "16px 20px 16px 20px",
-      },
-      "& .MuiSvgIcon-root": {
-        fontSize: 16,
-      },
-      "&:hover": {
-        color: "#0D0C0C",
-        backgroundColor: "#E5E9FA",
-      },
-      "&.Mui-focusVisible": {
-        backgroundColor: "#E5E9FA", // Remove the gray background on focus
-      },
-    },
-  },
-}));
+import { Doctor } from "../../../context/DoctorContext";
+import { SearchIcon } from "../../../img/svg/SearchIcon";
+import { useDoctorData } from "../../../context/DoctorDataProvider";
+import "./index.css";
 
 export type MenuListType = {
   icon: any;
@@ -67,26 +21,40 @@ enum SexOption {
   FEMALE = "F",
 }
 
-export const SexDropDown = React.memo(
-  ({ sex, setSex }: { sex: string; setSex: Function }) => {
-    const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-    const selectedOption =
-      sex === SexOption.MALE ? SexText.MALE : SexText.FEMALE;
-    const open = Boolean(anchorEl);
-    const handleClick = (event: React.MouseEvent<HTMLElement>) => {
-      setAnchorEl(event.currentTarget);
-    };
-    const handleClose = () => {
-      setAnchorEl(null);
-    };
-    const handleClickOption = (option: any) => {
-      setSex(option);
-      handleClose();
-    };
+export const SexDropDown = ({
+  sex,
+  setSex,
+}: {
+  sex: string;
+  setSex: Function;
+}) => {
+  const selectedOption = sex === SexOption.MALE ? SexText.MALE : SexText.FEMALE;
+  const [open, setOpen] = React.useState(false);
 
-    return (
-      <div className="box">
-        <div className="box-item">
+  const handleClick = (event: React.MouseEvent<HTMLElement>) => {
+    setOpen((prev) => !prev);
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleClickOption = (option: any) => {
+    setSex(option);
+    handleClose();
+  };
+
+  return (
+    <div className="box">
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          gap: "8px",
+          width: "100%",
+          cursor: "pointer",
+        }}
+      >
+        <div className="box-item" onClick={handleClick}>
           <div className="title">Giới tính</div>
           <div className="content">
             {sex ? (
@@ -97,34 +65,39 @@ export const SexDropDown = React.memo(
               </div>
             )}
 
-            <IconButton sx={{ height: "100%" }} onClick={handleClick}>
-              <ArrowDownIcon defaultColor="#0D0C0C" selectedColor="#0D0C0C" />
-            </IconButton>
+            <ArrowDownIcon defaultColor="#0D0C0C" selectedColor="#0D0C0C" />
           </div>
         </div>
-        <StyledMenu
-          id="demo-customized-menu"
-          MenuListProps={{
-            "aria-labelledby": "demo-customized-button",
-          }}
-          anchorEl={anchorEl}
-          open={open}
-          onClose={handleClose}
-        >
-          <MenuItem
-            onClick={() => handleClickOption(SexOption.MALE)}
-            disableRipple
-          >
-            {SexText.MALE}
-          </MenuItem>
-          <MenuItem
-            onClick={() => handleClickOption(SexOption.FEMALE)}
-            disableRipple
-          >
-            {SexText.FEMALE}
-          </MenuItem>
-        </StyledMenu>
+
+        <DropDown open={open}>
+          <div className="dropdown-menu-list">
+            <DropDownItem
+              key={SexText.MALE}
+              label={SexText.MALE}
+              onClick={() => handleClickOption(SexOption.MALE)}
+            />
+            <DropDownItem
+              key={SexText.FEMALE}
+              label={SexText.FEMALE}
+              onClick={() => handleClickOption(SexOption.FEMALE)}
+            />
+          </div>
+        </DropDown>
       </div>
-    );
-  }
-);
+    </div>
+  );
+};
+
+const DropDown = ({ open, children }: { open: boolean; children?: any }) => {
+  return (
+    <div className={`menu-container ${open ? "open" : ""}`}>{children}</div>
+  );
+};
+
+const DropDownItem = ({ label, onClick }: { label: string; onClick: any }) => {
+  return (
+    <div className="dropdown-item" onClick={onClick}>
+      {label}
+    </div>
+  );
+};
